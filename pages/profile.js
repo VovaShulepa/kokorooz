665fc3820ece140e74d62392c72faa7ad64aa629
Volodymyr Shulepa <106019079+VovaShulepa@users.noreply.github.com>
2023-11-21 16:36:43 +0200

    add mongo

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
 import { getSession } from "next-auth/react";
 
-export default function Profile() {
+export default function Profile({ session }) {
   return (
     <section className="container mx-auto text-center">
       <h3 className="text-4xl font-bold">Profile Page</h3>
+
+      {session ? (
+        <div>
+          <p>Welcome, {session.user.name}!</p>
+          <p>Email: {session.user.email}</p>
+        </div>
+      ) : (
+        <p>Please sign in to view your profile.</p>
+      )}
 
       <Link href={"/"}>Home Page</Link>
     </section>
@@ -22,7 +31,7 @@ export async function getServerSideProps({ req }) {
       },
     };
   }
-  // authorize user return session
+
   return {
     props: { session },
   };
