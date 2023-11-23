478e16d5c8ac0a3e3714f82d080e1beeac2bebc8
Volodymyr Shulepa <106019079+VovaShulepa@users.noreply.github.com>
2023-11-23 17:32:00 +0200

    add console for errors mongo

--- database/conn.js
+++ database/conn.js
@@ -4,14 +4,15 @@ const connectMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
     const { connection } = await mongoose.connect(process.env.MONGO_URL);
-    // const { connection } = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(connectMongo);
 
     if (connection.readyState === 1) {
+      console.log("MongoDB Connected Successfully");
       return Promise.resolve(true);
     }
   } catch (error) {
+    console.error("MongoDB Connection Error:", error);
     return Promise.reject(error);
   }
   console.log(connectMongo);
